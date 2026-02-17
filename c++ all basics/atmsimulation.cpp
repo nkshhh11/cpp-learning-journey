869f16b8f0ab89d3c8199e2be9039fb72c32bde8
nkshhh11 <pv9696589211@gmail.com>
2026-02-17 22:04:46 +0530

    my first c++ project which is atm machine project

---
+++ c++ all basics/atmsimulation.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int balance = 10000;
+   int pin=1234;
+   int enteredpin;
+   int choice;
+   int amount;
+   cout<<"enter your pin : ";
+   cin>>enteredpin;
+   if(enteredpin == pin)
+   {
+        do{
+            cout<<"\n\n==MCHINE==\n";
+            cout<<"1. check balance\n";
+            cout<<"2. withdraw balance\n";
+            cout<<"3. deposite balance\n";
+            cout<<"4. Exit\n";
+            cout<<"enter your choice : ";
+            cin>>choice;
+
+            switch(choice){
+                case 1:
+                    cout<<"balance is : "<<balance;
+                    break;
+                case 2:
+                    cout<<"enter amount to withdraw : ";
+                    cin>>amount;
+
+                    if(amount <= balance){
+                        balance=balance-amount;
+                        cout<<"withdrawl succesfull!"<<endl;
+                        cout<<"\nRemaining balance : "<<balance;
+                    }
+
+                    else{
+                        cout<<"insufficient balance!";
+                    }
+                    break;
+                case 3:
+                    cout<<"enter amount to deposite : ";
+                    cin>>amount;
+                    
+                    balance = balance + amount ;
+                    cout<<"deposit successfull!"<<endl;
+                    cout<<"your current updated balance is : "<<balance;
+                    break;
+                case 4:
+                    cout<<"<<<<<<<<< Thank you for using ATM >>>>>>>>";
+                    break;
+                default:
+                    cout<<"Invalid choice";
+            }
+
+    }while(choice != 4);
+   }
+    else{
+    cout<<"Wrong pin";
+}
+}"     "
