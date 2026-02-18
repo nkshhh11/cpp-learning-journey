38e18f29f4ac1aa191f63ca99dee64e71bf1a9d8
nkshhh11 <pv9696589211@gmail.com>
2026-02-18 23:10:16 +0530

    half pyramid of star and numbers

---
+++ complete c++/halfpyramid.cpp
@@ -0,0 +1,33 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//    int a;
+//    cout<<"enter the half pyramid length : ";
+//    cin>>a;
+//    for(int i=1 ; i<=a ; i++){
+//     for(int j=1 ; j<=i ; j++){
+//     cout<<j<<" ";
+//     }
+//     cout<<endl;
+//    }
+// }
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int a;
+   cout<<"enter the half pyramid length : ";
+   cin>>a;
+   for(int i=1 ; i<=a ; i++){
+    for(int j=1 ; j<=i ; j++){
+    cout<<" * ";
+    }
+    cout<<endl;
+   }
+}
+
