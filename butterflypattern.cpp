814bf1e04394496dc69ca6280d343bacc31444ab
nkshhh11 <pv9696589211@gmail.com>
2026-02-19 02:01:51 +0530

    butterfly pattern printing

---
+++ butterflypattern.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    // 🔺 Upper Half
+    for(int i = 1; i <= n; i++) {
+
+        // Left stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        // Middle spaces
+        for(int j = 1; j <= 2*(n - i); j++) {
+            cout << " ";
+        }
+
+        // Right stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // 🔻 Lower Half
+    for(int i = n; i >= 1; i--) {
+
+        // Left stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        // Middle spaces
+        for(int j = 1; j <= 2*(n - i); j++) {
+            cout << " ";
+        }
+
+        // Right stars
+        for(int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
