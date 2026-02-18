476b889f80dfb70099256f50adec3a6a104ca3b0
nkshhh11 <pv9696589211@gmail.com>
2026-02-18 23:35:26 +0530

    all type of inverted pyramid

---
+++ complete c++/invertedhalfpyramid.cpp
@@ -0,0 +1,79 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//    int a;
+//    cout<<"enter the half pyramid length : ";
+//    cin>>a;
+//    for(int i=a ; i>=1 ; i--){
+//     for(int j=a ; j>=a-i+1 ; j--){
+//         cout<<" * ";
+//     }
+//     cout<<endl;
+//    }
+// }
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//    int a;
+//    cout<<"enter the half pyramid length : ";
+//    cin>>a;
+//    for(int i=a ; i>=1 ; i--){
+//     for(int j=a ; j>=a-i+1 ; j--){
+//         cout<<j<<" ";
+//     }
+//     cout<<endl;
+//    }
+// }
+
+
+
+
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int a;
+   cout<<"enter the half pyramid length : ";
+   cin>>a;
+   for(int i=a ; i>=1 ; i--){
+    for(int j=a ; j>=a-i+1 ; j--){
+        cout<<(char)(i+64)<<" ";
+    }
+    cout<<endl;
+   }
+}
+
+
+
+ 
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//    int a;
+//    cout<<"enter the half pyramid length : ";
+//    cin>>a;
+//    for(int i=a ; i>=1 ; i--){
+//     for(int j=a ; j>=a-i+1 ; j--){
+//      cout<<(char)(j+64)<<" ";
+//     }
+//     cout<<endl;
+//    }
+// }
+
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,14 +1,1 @@
-#include <bits/stdc++.h>
-// using namespace std;
-// int main() {
-//    int x ;
-//    cout<<"enter the numbre : ";
-//    cin>>x;
-//    int sum = 0;
-//    while(x!=0){
-//     int ld = x%10;
-//     x = x/10;
-//     sum += ld;
-//    }
-//    cout<<sum;
-// }
+ cout<<(char)(j+64)<<" ";
