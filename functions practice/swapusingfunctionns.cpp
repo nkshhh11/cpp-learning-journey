4c7729fe48d6d570ef34db1284e1159ae3d8e494
nkshhh11 <pv9696589211@gmail.com>
2026-02-20 00:48:06 +0530

    call refrence and call by value both swap program

---
+++ functions practice/swapusingfunctionns.cpp
@@ -0,0 +1,56 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// void swap(int &a , int &b){
+//     int temp;
+//     temp=a;
+//     a=b;
+//     b=temp;
+// }
+// int main(){
+//     int a , b;
+//     cout<<"enter first number : ";
+//     cin>>a;
+//     cout<<"enter the second number : ";
+//     cin>>b;
+//     swap(a , b);
+//     cout<<"swapped number is : "<<endl;
+//     cout<<a<<endl;
+//     cout<<b<<endl;
+// }
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+void swap(int a, int b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "Inside function : " << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+}
+
+int main() {
+    int a, b;
+
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    swap(a, b);
+
+    cout << "after swap : " << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    return 0;
+}
