a07480c2ab16d69ebf4f8ff5a9fdf8dbfa9d311f
nkshhh11 <pv9696589211@gmail.com>
2026-07-30 09:03:45 +0530

    +

--- complete c++/class.cpp
+++ complete c++/class.cpp
@@ -281,10 +281,73 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0 ; i<n ; i++){
+//         cin>>arr[i];
+//     }
+//     int hash[13]={0};
+//     for(int i=0 ; i<n ; i++){
+//         hash[arr[i]]+=1;
+//     }
+//     int q;
+//     cin>>q;
+//     while(q--){
+//         int number;
+//         cin>>number;
+// cout<<hash[number]<<endl;
+//     }
+// }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//    string s;
+//    cin>>s;
+//    int q;
+//    cin>>q;
+//    int hash[26]={0};
+//    for(int i=0 ; i<s.size() ; i++){
+//     hash[s[i]-'a']++;
+//    }
+//    while(q--){
+//     char c;
+//     cin>>c;
+//     cout<<hash[c-'a'];
+//    }
+//     return 0;
+// }
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n;
-    cin>>n;
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0 ; i<n ; i++){
+    cin>>arr[i];
+  }
+  map<int , int>mpp;
+  for(int i=0 ; i<n ; i++){
+    mpp[arr[i]]++;
+  }
+  int q;
+  cin>>q;
+  while(q--){
+    int number;
+    cin>number;
+    cout<<map[number]<<endl;
+  }
 }
