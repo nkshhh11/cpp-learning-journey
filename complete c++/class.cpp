d75f32ef38fadb83e0b5dded56b0ee52ae84a0b0
nkshhh11 <pv9696589211@gmail.com>
2026-08-03 22:59:22 +0530

    D3

--- complete c++/class.cpp
+++ complete c++/class.cpp
@@ -588,27 +588,150 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout<<"enter the size of an array : ";
+//     cin>>n;
+//     int arr[n];
+//     int max=0;
+//     for(int i=0 ; i<n ; i++){
+//         cin>>arr[i];
+//     }
+//     for(int i=0 ; i<n ; i++){
+//         if(arr[i]>max)
+//         max=arr[i];
+//     }
+//     int slarge st=-1;
+//     for(int i=0 ; i<n ; i++){
+//         if(arr[i]>slargest && arr[i]!=max)
+//         slargest=arr[i];
+//     }
+//     cout<<slargest;
+//     return 0;
+// }
+
+
+
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// // Function to find the second smallest element in the array
+// int secondSmallest(int arr[], int n) {
+//     // Edge case: if the array has fewer than 2 elements
+//     if (n < 2)
+//         return -1;
+
+//     int small = INT_MAX;
+//     int second_small = INT_MAX;
+
+//     // Loop through the array to find the second smallest element
+//     for (int i = 0; i < n; i++) {
+//         // Update the smallest and second smallest values
+//         if (arr[i] < small) {
+//             second_small = small;
+//             small = arr[i];
+//         } 
+//         else if (arr[i] < second_small && arr[i] != small) {
+//             second_small = arr[i];
+//         }
+//     }
+//     return second_small; // Return the second smallest element
+// }
+
+// // Function to find the second largest element in the array
+// int secondLargest(int arr[], int n) {
+//     // Edge case: if the array has fewer than 2 elements
+//     if (n < 2)
+//         return -1;
+
+//     int large = INT_MIN, second_large = INT_MIN;
+
+//     // Loop through the array to find the second largest element
+//     for (int i = 0; i < n; i++) {
+//         // Update the largest and second largest values
+//         if (arr[i] > large) {
+//             second_large = large;
+//             large = arr[i];
+//         } 
+//         else if (arr[i] > second_large && arr[i] != large) {
+//             second_large = arr[i];
+//         }
+//     }
+//     return second_large; // Return the second largest element
+// }
+
+// int main() {
+//     // Array of elements
+//     int arr[] = {1, 2, 4, 7, 7, 5};  
+
+//     // Calculate the size of the array
+//     int n = sizeof(arr) / sizeof(arr[0]);
+
+//     // Find the second smallest and second largest elements
+//     int sS = secondSmallest(arr, n);
+//     int sL = secondLargest(arr, n);
+
+//     // Output the results
+//     cout << "Second smallest is " << sS << endl;
+//     cout << "Second largest is " << sL << endl;
+
+//     return 0;
+// }
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout << "Enter the size of array: ";
+//     cin >> n;
+
+//     int arr[n];
+
+//     cout << "Enter the elements: ";
+
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     bool sorted = true;
+
+//     for(int i = 1; i < n; i++) {
+//         if(arr[i] < arr[i - 1]) {
+//             sorted = false;
+//             break;
+//         }
+//     }
+
+//     if(sorted)
+//         cout << "True";
+//     else
+//         cout << "False";
+
+//     return 0;
+// } 
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n;
-    cout<<"enter the size of an array : ";
-    cin>>n;
-    int arr[n];
-    int max=0;
-    for(int i=0 ; i<n ; i++){
-        cin>>arr[i];
-    }
-    for(int i=0 ; i<n ; i++){
-        if(arr[i]>max)
-        max=arr[i];
-    }
-    int slarge st=-1;
-    for(int i=0 ; i<n ; i++){
-        if(arr[i]>slargest && arr[i]!=max)
-        slargest=arr[i];
-    }
-    cout<<slargest;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    // your code here
+    int a=10;
+    cout<<a;
     return 0;
 }
+
+
