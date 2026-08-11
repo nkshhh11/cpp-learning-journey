e71d1f0f9648edd917c734bbe58ef70b6cefa47d
nkshhh11 <pv9696589211@gmail.com>
2026-08-11 23:44:53 +0530

    practice c++

--- complete c++/class.cpp
+++ complete c++/class.cpp
@@ -747,21 +747,77 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// void rotate(int n , int arr[] ,  int d){
+//    d=d%n;
+//    int temp[d];
+//    for(int i=0 ; i<d ;i++){
+//       temp[i]=arr[i];
+//    }
+//    for(int i=d ; i<n ; i++){
+//       arr[i-d]=arr[i];
+//    }
+//    for(int i=n-d ; i<n ;i++){
+//       arr[i]=temp[i-(n-d)];
+//    }
+// }
+// int main() {
+//    int n;
+//    cin>>n;
+//    int arr[n];
+//    for(int i=0 ; i<n ; i++){
+//     cin>>arr[i];
+//    }
+//    int d;
+//    cin>>d;
+//    rotate(n , arr , d);
+//    for(int i=0 ; i<n ; i++){
+//       cout<<arr[i]<<"  ";
+//    }
+// }
+
+
+
+
+
+
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void rotate(int n , int arr[]){
+// vector<int>temp;
+// for(int i=0 ; i<n ; i++){
+//    if(arr[i]!=0)
+//    temp.push_back(arr[i]);
+// }
+
+// }
+// int main() {
+//    int n;
+//    cin>>n;
+//    int arr[n];
+//    for(int i=0 ; i<n ; i++){
+//     cin>>arr[i];
+//    }
+//    rotate(n , arr);
+//    for(int i=0 ; i<n ; i++){
+//       cout<<arr[i]<<"  ";
+//    }
+// }
+
+
+
+
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
-void rotate(int n , int arr[] ,  int d){
-   d=d%n;
-   int temp[d];
-   for(int i=0 ; i<d ;i++){
-      temp[i]=arr[i];
-   }
-   for(int i=d ; i<n ; i++){
-      arr[i-d]=arr[i];
-   }
-   for(int i=n-d ; i<n ;i++){
-      arr[i]=temp[i-(n-d)];
-   }
-}
 int main() {
    int n;
    cin>>n;
@@ -769,14 +825,14 @@ int main() {
    for(int i=0 ; i<n ; i++){
     cin>>arr[i];
    }
-   int d;
-   cin>>d;
-   rotate(n , arr , d);
+   int num=0;
+     for(int i=0 ; i<n ; i++){
+           num = arr[i];
    for(int i=0 ; i<n ; i++){
       cout<<arr[i]<<"  ";
    }
 }
-
+}
 
 
 
